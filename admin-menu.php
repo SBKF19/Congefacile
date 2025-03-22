9210b4a209cbd9d701bd519849264a6c55a56dce
Eva Valenti <b.beta.crystal@gmail.com>
2025-03-22 17:59:00 +0100

    menu admin sans profil

---
+++ admin-menu.php
@@ -0,0 +1,43 @@
+<?php
+        include "header.php";
+?>
+<div class="side-menu">
+        <div class="side-menu-pages">
+                <div class="onglet">
+                        <a>Accueil</a>
+                </div>
+                <div class="onglet">
+                        <a>Demandes en attente</a>
+                </div>
+                <div class="onglet">
+                        <a>Historique des demandes</a>
+                </div>
+                <div class="onglet">
+                        <a>Mon équipe</a>
+                </div>
+                <div class="onglet">
+                        <a>Statistiques</a>
+                </div>
+                <div class="onglet">
+                        <div></div>
+                </div>
+                <div class="onglet">
+                        <a>Mes informations</a>
+                </div>
+                <div class="onglet">
+                        <a>Mes préférences</a>
+                </div>
+                <div class="onglet">
+                        <a>Administration</a>
+                </div>
+                <div class="onglet">
+                        <a>Déconnexion</a>
+                </div>
+        </div>
+
+
+
+        <div class="side-menu-profile">
+
+        </div>
+</div>
