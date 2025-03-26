2b9b22428237e63423ab793c47dbd0bba77aafd1
Abida Bibi <abidabibi60100@gmail.com>
2025-03-26 17:05:26 +0100

    test

---
+++ accueil.php
@@ -0,0 +1,35 @@
+<?php 
+
+?>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="accueilPage.css">
+    <link href='https://fonts.googleapis.com/css?family=Epilogue' rel=''>
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="container">
+            <h1>CongéFacile</h1>
+            <p>CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l’entreprise. <br> Plus besoin d’échanges interminables ou de formulaires papier : en quelques clics, vous pouvez gérer vos absences en toute transparence et simplicité.</p>
+            
+            <h2>Etapes</h2>
+            <div class="timeline-container">
+                <div class="timeline">
+                    <div class="etape active" id="etape1">1</div>
+                    <div class="line"></div>
+                    <div class="etape" id="etape2">2</div>
+                    <div class="line"></div>
+                    <div class="etape" id="etape3">3</div>
+                </div>
+                <div class="descriptions">
+                    <div class="description-box">J’effectue ma demande de congés</div>
+                    <div class="description-box">Mon manager valide ou refuse la demande</div>
+                    <div class="description-box">Je consulte l’historique de mes demandes</div>
+                </div>
+            </div>
+            <div class="footer">
+                <p>En cas de difficulté avec l’application, veuillez envoyer un email à <a href="mailto:contact@mentalworks.fr">contact@mentalworks.fr</a>.</p>
+            </div>
+        </div>
+</body>
