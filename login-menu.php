b0f3b59a055b4ff08a650126447a6cdf891e8ab7
Eva Valenti <b.beta.crystal@gmail.com>
2025-03-27 19:19:32 +0100

    header fix

--- login-menu.php
+++ login-menu.php
@@ -2,7 +2,6 @@
         include "header.php";
 ?>
 <div class="cont2">
-<div class="side-menu-below">
 <div class="side-menu">
         <div class="side-menu-pages">
                 <div class="onglet">
