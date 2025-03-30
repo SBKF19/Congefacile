c36994831b43064500e1dc0f4a8f6e0db149da8a
Eva Valenti <b.beta.crystal@gmail.com>
2025-03-30 20:51:46 +0200

    main

---
+++ includes/login-menu.php
@@ -0,0 +1,15 @@
+<?php
+        include "header.php";
+?>
+<div class="cont2">
+<div class="side-menu">
+        <div class="side-menu-pages">
+                <div class="onglet">
+                        <a>Connexion</a>
+                </div>
+        </div>
+
+
+
+        <div class="side-menu-profile"></div>
+</div>
