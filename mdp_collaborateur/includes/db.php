558a31ac3bfcdfd81d115604a15c09ef4f640cd5
Kecy Platel <kecy.platel@gmail.com>
2025-04-25 16:40:41 +0200

    syncronisation de la branche Kecy et la branche Main

--- mdp_collaborateur/includes/db.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-    
-    $user = "root";
-    $pass = "";
-    $dbName = "congefacile";
-    $connexion = new \PDO("mysql:host=127.0.0.1;dbname=$dbName;charset=UTF8", $user, $pass);
-   
-?>
