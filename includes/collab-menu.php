b0ee13d9862614d1f5c6b16bc9b57e1aaf52640a
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-05-10 17:52:24 +0200

    Update collab-menu.php

--- includes/collab-menu.php
+++ includes/collab-menu.php
@@ -9,7 +9,7 @@ include 'includes/database.php';
                                 <a href="accueil.php">Accueil</a>
                         </div>
                         <div class="onglet">
-                                <a href="demande_conge_test.php">Nouvelle demande</a>
+                                <a href="nouvelle_demande.php">Nouvelle demande</a>
                         </div>
                         <div class="onglet">
                                 <a href="historique_des_demandes.php">Historique des demandes</a>
