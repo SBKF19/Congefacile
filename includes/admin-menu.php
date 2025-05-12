41f219c819bb56fd175b313e474fde5a42b16c47
Eva Valenti <b.beta.crystal@gmail.com>
2025-05-12 08:06:23 +0200

    renommage, calendrier css

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -1,5 +1,13 @@
 <?php
 include "header.php";
+include 'database.php';
+$requete = $connexion->prepare('SELECT COUNT(collaborator_id) AS demande_en_attente FROM request JOIN user
+ON collaborator_id = user.person_id JOIN person ON user.person_id = person.id WHERE request.answer IS NULL AND person.manager_id = :id');
+
+$requete->bindParam(':id', $_SESSION['utilisateur']['person_id']);
+$requete->execute();
+$demande_en_attente = $requete->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
 <div class="cont2">
@@ -9,23 +17,23 @@ include "header.php";
                                 <a href="accueil.php">Accueil</a>
                         </div>
                         <div class="onglet">
-                                <a>Demandes en attente</a>
+                                <a href="demandes_en_attente.php">Demandes en attente</a>
                                 <div class="side-menu-counter">
-                                        <p>0</p>
+                                        <p><?php echo $demande_en_attente['demande_en_attente'] ?></p>
                                 </div>
                         </div>
                         <div class="onglet">
-                                <a>Historique des demandes</a>
+                                <a href="">Historique des demandes</a>
                         </div>
                         <div class="onglet">
-                                <a>Mon équipe</a>
+                                <a href="mon_equipe.php">Mon équipe</a>
                         </div>
                         <div class="onglet">
                                 <a>Statistiques</a>
                         </div>
                         <div class="menu-lign"></div>
                         <div class="onglet">
-                                <a>Mes informations</a>
+                                <a href="mes_informations.php">Mes informations</a>
                         </div>
                         <div class="onglet">
                                 <a>Mes préférences</a>
@@ -36,7 +44,7 @@ include "header.php";
                         </div>
                         <div class="hidden-menu">
                                 <div>
-                                        <a>Types de demandes</a>
+                                        <a href="types_de_demandes.php">Types de demandes</a>
                                 </div>
                                 <div>
                                         <a>Directions/Services</a>
@@ -53,15 +61,12 @@ include "header.php";
                         </div>
                 </div>
 
-
-
                 <div class="side-menu-profile">
                         <div class="side-menu-profile-image">
-                                <img src="man.png" />
+                                <img src="images/man.png" />
                         </div>
                         <div class="side-menu-profile-text">
-                                <p class="username">Placeholder</p>
-                                <p class="job">Placeholder</p>
+                                <?php include 'name_and_role.php'; ?>
                         </div>
                 </div>
         </div>
