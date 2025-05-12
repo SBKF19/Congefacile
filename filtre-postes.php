47e6b240d08f0159e93b5a37f99ac966523e6d3c
Nicolas Le-Goffic <legoffic.nicolas@gmail.com>
2025-05-12 09:36:18 +0200

    modification du filtre poste en cours

--- filtre-postes.php
+++ filtre-postes.php
@@ -41,32 +41,32 @@ for ($i = 0; $i < Count($postes1); $i++){
                 <div class="side-menu-profile filterBar">
                         <div class="filterMargin">
                                 <label class="label-select">Nom du poste</label>
-                                <input class="large-filter filter" type="text">
+                                <input class="large-filter filter" type="text" id="nomPoste">
                         </div>
                         <div class="filterMargin">
                                 <label class="label-select">Nb personnes liées</label>
-                                <input class="medium-filter filter" type="text">
+                                <input class="medium-filter filter" type="text" id="nbPersLiees">
                         </div>
                 </div>
-                <div class="list_conge">
-                        <div class="congeType">
+                <?php
+                for ($i = 0; $i < Count($postes1); $i++){ ?>
+                <div class="congeType card">
+                    <div class="list_conge">
+                        <div class="Type1">
                             <?php
-                                for ($i = 0; $i < Count($postes1); $i++){
-                                    if( $i === Count($postes1)-1){ ?>
-                                    <div class="filter-info-large">
-                                        <p class="break-details"><?= $postes1[$i]['name']?></p>
-                                    </div>
-                                    <?php } else{ ?>
-                                        <div class="filter-info-large filterBorderBottom">
-                                        <p class="break-details"><?= $postes1[$i]['name']?></p>
-                                        </div>
-                                    <?php }
-                                }
-                            ?>
-                        </div>
-                        <div class="congeType">
+                            if( $i === Count($postes1)-1){ ?>
+                            <div class="filter-info-type">
+                                <p class="break-details"><?= $postes1[$i]["name"];?></p>
+                            </div>
+                            <?php } else{ ?>
+                                <div class="filter-info-type filterBorderBottom">
+                                <p class="break-details"><?= $postes1[$i]["name"];?></p>
+                                </div>
+                            <?php }
+                        ?>
+                            </div>
+                        <div class="Type2">
                             <?php
-                                for ($i = 0; $i < Count($tab); $i++){
                                     if( $i === Count($tab)-1){ ?>
                                     <div class="filter-info-medium">
                                         <p class="break-details"><?= $tab[$i]?></p>
@@ -76,12 +76,10 @@ for ($i = 0; $i < Count($postes1); $i++){
                                         <p class="break-details"><?= $tab[$i]?></p>
                                         </div>
                                     <?php }
-                                }
                             ?>
                         </div>
-                        <div class="congeType">
+                        <div class="">
                 <?php
-                for ($i = 0; $i < Count($postes1); $i++){
                     if( $i === Count($postes1)-1){ ?>
                     <div class="filter-info-details">
                         <button class="details-button" href="*"><a>Détails</a></button>
@@ -90,15 +88,52 @@ for ($i = 0; $i < Count($postes1); $i++){
                     <div class="filter-info-details filterBorderBottom">
                         <button class="details-button" href="*"><a>Détails</a></button>
                     </div>
-                    <?php }
-                } ?>
+                    <?php } ?>
                         </div>
                 </div>
-
+            <?php } ?>
         </div>
 
 </div>
 </div>
+<script>
+    const searchbarNomPoste = document.querySelector("#nomPoste");
+    const searchbarNbPersLiees = document.querySelector("#nbPersLiees");
+
+    searchbarNomPoste.addEventListener("keyup", (e) =>{
+        const searchedLetters = e.target.value;
+        const typeElement = document.querySelectorAll(".Type1");
+        const cards = document.querySelectorAll(".card");
+        filterElements(searchedLetters,typeElement,cards);
+    });
+
+    searchbarNbPersLiees.addEventListener("keyup", (e) =>{
+        const searchedLetters = e.target.value;
+        const typeElement = document.querySelectorAll(".Type2");
+        const cards = document.querySelectorAll(".card");
+        filterElements(searchedLetters,typeElement,cards);
+    });
+
+    function filterElements(letters, Type, elements){
+        if(letters.length > 0){
+            for (let i=0; i < elements.length; i++){
+                if(Type[i].textContent.includes(letters) || Type[i].textContent.toLowerCase().includes(letters)){
+                    elements[i].style.display = "flex";
+                } else{
+                    elements[i].style.display = "none";
+                }
+            }
+        }
+        else{
+            for (let i=0; i < elements.length; i++){
+                if(Type[i].textContent.toLowerCase().includes(letters)){
+                    elements[i].style.display = "flex";
+                }
+            }
+        }
+    }
+
+</script>
 <?php
         include "includes/footer.php";
 ?>
