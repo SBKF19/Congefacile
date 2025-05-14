592927fec134ea3ff5f42e50852d84f096969e55
Kecy Platel <kecy.platel@gmail.com>
2025-05-14 16:40:55 +0200

    debug

--- mes_informations_collab.php
+++ mes_informations_collab.php
@@ -8,12 +8,12 @@
         <div class="date">
                 <div>
                     <label for="nom" class="label-input">Nom de famille</label>
-                    <input type="text" id="nom"  class="defaultbox" readonly>
+                    <input type="text" id="nom"  class="defaultbox-input defaultbox" readonly>
                 </div>
 
                 <div>
                     <label for="prenom" class="label-input">Prénom</label>
-                    <input type="text" id="prenom"  class="defaultbox" readonly>
+                    <input type="text" id="prenom"  class=" defaultbox-input defaultbox" readonly>
                     
                 </div>
         </div>
@@ -21,13 +21,13 @@
 <br>
         <div>
             <label for="mail" class="label-input">Adresse email</label>
-            <input type="email" id="mail"  class="defaultbox" readonly>
+            <input type="email" id="mail"  class="defaultbox-input defaultbox" readonly>
         </div>
 <br>
         <div class="date">
             <div>
                 <label for="direction" class="label-input">Direction/Service</label>
-                <select type="text" id="direction" class="defaultbox" readonly>
+                <select type="text" id="direction" class="defaultbox-input defaultbox" readonly>
                     <option value="" ></option>
                 </select>
             </div>
@@ -35,7 +35,7 @@
                 
             <div>
                 <label for="poste" class="label-input">Poste</label>
-                <select type="text" id="poste" class="defaultbox" readonly>
+                <select type="text" id="poste" class="defaultbox-input defaultbox" readonly>
                     <option value="" ></option>
                 </select>
             </div>
@@ -43,7 +43,7 @@
 <br>
         <div>
             <label for="Manager" class="label-input">Manager</label>
-            <select type="text" id="manager" class="defaultbox" readonly>
+            <select type="text" id="manager" class="defaultbox-input defaultbox" readonly>
                 <option value="" ></option>
             </select>
         </div>
@@ -55,16 +55,16 @@
 
     <form>
         <label for="password" class="label-input">Mot de passe actuel</label>
-        <input type="password" id="password" name="password" class="label-select"><br>
+        <input type="password" id="password" name="password" class="label-field"><br>
         <div class="date">
             
         <div>
                 <label for="newpass" class="label-input">Nouveau mot de passe</label>
-                <input type="password" id="newpass" name="newpass" class="label-select">
+                <input type="password" id="newpass" name="newpass" class="label-fixed-value">
             </div>
             <div>
                 <label for="confirmpass" class="label-input">Confirmation du mot de passe</label>
-                <input type="password" id="confirmpass" name="confirmpass" class="label-select">
+                <input type="password" id="confirmpass" name="confirmpass" class="label-field">
             </div>
         </div>
         <br>
