331843938b35965fed57687f40845d4071a8f39c
Eva Valenti <b.beta.crystal@gmail.com>
2025-05-17 12:11:08 +0200

    burger menu

--- includes/login-menu.php
+++ includes/login-menu.php
@@ -1,6 +1,17 @@
 <?php
         include "header.php";
 ?>
+<ul class="menu">
+    <li><a class="menuItem" href="#">Home</a></li>
+    <li><a class="menuItem" href="#">Profile</a></li>
+    <li><a class="menuItem" href="#">About</a></li>
+    <li><a class="menuItem" href="#">Contacts</a></li>
+  </ul>
+  <button class="hamburger">
+    <!-- material icons https://material.io/resources/icons/ -->
+    <i class="menuIcon material-icons">menu</i>
+    <i class="closeIcon material-icons">close</i>
+  </button>
 <div class="cont2">
 <div class="side-menu">
         <div class="side-menu-pages">
@@ -8,8 +19,6 @@
                         <a href="connexion.php">Connexion</a>
                 </div>
         </div>
-
-
-
         <div class="side-menu-profile"></div>
 </div>
+<script src="includes/burger_menu.js"></script>
